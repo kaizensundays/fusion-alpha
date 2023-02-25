e59da67d55520f0aac32b2b90d4cc6c3afd95229
KaizenSundays <kaizensundays@localhost>
2023-02-25 12:03:18 -0500

    [ fusion ] NodeState

--- fusion-mu/src/main/java/com/kaizensundays/particles/fusion/mu/NodeState.kt
+++ fusion-mu/src/main/java/com/kaizensundays/particles/fusion/mu/NodeState.kt
@@ -1,12 +1,14 @@
 package com.kaizensundays.particles.fusion.mu
 
 import org.apache.ignite.Ignite
+import org.apache.ignite.cluster.ClusterNode
 import org.apache.ignite.events.DiscoveryEvent
 import org.apache.ignite.events.Event
 import org.apache.ignite.lang.IgnitePredicate
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.util.concurrent.atomic.AtomicBoolean
+import javax.annotation.PostConstruct
 
 /**
  * Created: Monday 2/20/2023, 12:10 PM Eastern Time
@@ -17,7 +19,52 @@ class NodeState(private val ignite: Ignite) : IgnitePredicate<Event> {
 
     private val logger: Logger = LoggerFactory.getLogger(javaClass)
 
+    companion object {
+        const val CLUSTER_VOTES_PROP = "cluster.votes"
+        const val CLUSTER_QUORUM_PROP = "cluster.quorum"
+    }
+
     val active = AtomicBoolean(false)
+
+    fun getIntAttribute(node: ClusterNode, name: String, default: Int): Int {
+        return node.attribute<String>(name)?.toInt() ?: default
+    }
+
+    fun votes(node: ClusterNode): Int {
+        return getIntAttribute(node, CLUSTER_VOTES_PROP, 0)
+    }
+
+    fun quorum(node: ClusterNode): Int {
+        return getIntAttribute(node, CLUSTER_QUORUM_PROP, 0)
+    }
+
+    fun quorum(nodes: Collection<ClusterNode>): Int {
+
+        val set = nodes.map { node -> quorum(node) }.toSet()
+
+        if (set.size != 1) {
+            throw IllegalStateException("Configuration error")
+        }
+
+        return set.first()
+    }
+
+    fun isActive(nodes: Collection<ClusterNode>): Boolean {
+
+        val votes = nodes.sumOf { node -> votes(node) }
+
+        val quorum = quorum(nodes)
+
+        return votes >= quorum
+    }
+
+    private fun print(nodes: Collection<ClusterNode>) {
+        nodes.forEach { node ->
+            val votes = votes(node)
+            val id = node.id().toString()
+            logger.info("{}:{}", id, votes)
+        }
+    }
 
     override fun apply(event: Event): Boolean {
 
@@ -25,14 +72,25 @@ class NodeState(private val ignite: Ignite) : IgnitePredicate<Event> {
 
         if (event is DiscoveryEvent) {
             val nodes = event.topologyNodes()
-            nodes.forEach { node ->
-                val votes = node.attribute<String>("fusion-mu.votes")?.toInt() ?: 0
-                val id = node.id().toString()
-                logger.info("{}:{}", id, votes)
-            }
+
+            active.set(isActive(nodes))
+
+            print(nodes)
         }
 
         return true
+    }
+
+    @PostConstruct
+    fun start() {
+
+        val nodes = ignite.cluster().forServers().nodes()
+
+        print(nodes)
+
+        active.set(isActive(nodes))
+
+        logger.info("Stared({${active.get()}})")
     }
 
 }
---
+++ fusion-mu/src/test/java/com/kaizensundays/particles/fusion/mu/NodeStateTest.kt
@@ -0,0 +1,48 @@
+package com.kaizensundays.particles.fusion.mu
+
+import org.apache.ignite.Ignite
+import org.apache.ignite.spi.discovery.tcp.internal.TcpDiscoveryNode
+import org.junit.Test
+import org.mockito.kotlin.mock
+import java.util.*
+import kotlin.test.assertFalse
+import kotlin.test.assertTrue
+
+/**
+ * Created: Saturday 2/25/2023, 11:16 AM Eastern Time
+ *
+ * @author Sergey Chuykov
+ */
+class NodeStateTest {
+
+    private val ignite: Ignite = mock()
+
+    private val state = NodeState(ignite)
+
+    private fun node(votes: Int, quorum: Int): TcpDiscoveryNode {
+        val node = TcpDiscoveryNode(
+            UUID.randomUUID(),
+            emptyList<String>(), emptyList<String>(),
+            0, mock(), mock(), ""
+        )
+        node.attributes = mapOf(
+            NodeState.CLUSTER_VOTES_PROP to votes.toString(),
+            NodeState.CLUSTER_QUORUM_PROP to quorum.toString(),
+        )
+        return node
+    }
+
+    @Test
+    fun isActive() {
+
+        assertTrue(state.isActive(listOf(node(1, 1))))
+
+        assertTrue(state.isActive(listOf(node(1, 2), node(1, 2), node(1, 2))))
+        assertTrue(state.isActive(listOf(node(1, 2), node(1, 2))))
+        assertFalse(state.isActive(listOf(node(1, 2))))
+
+        assertTrue(state.isActive(listOf(node(2, 2), node(1, 2))))
+        assertTrue(state.isActive(listOf(node(2, 2))))
+    }
+
+}
