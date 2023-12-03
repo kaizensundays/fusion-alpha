b75d5776e2b67f68f329ecb1c62ba286e0bd428d
KaizenSundays <kaizensundays@localhost>
2023-12-03 10:02:51 -0500

    [ fusion ] Spring Boot 2.2.13

--- sonar.cmd
+++ sonar.cmd
@@ -1,4 +1,4 @@
 
-set JAVA_HOME=%JAVA_11_HOME%
+set JAVA_HOME="%JAVA_17_HOME%"
 
-mvn verify sonar:sonar %SONAR_OPTS_FUSION_ALPHA% -Dsonar.branch.name=dev1 -P sonar
+mvn verify sonar:sonar %SONAR_OPTS_FUSION_ALPHA% -Dsonar.branch.name=dev -P sonar
